6fb08ebe1fc6ab99b8debf9052ab48882002be5b
PlanificacionII <jorge.navea@uda.cl>
2026-09-24 16:20:50 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -138,20 +138,20 @@ for i in range(1, cant_sondajes + 1):
                 
                 if factor_forma > 0.45:
                     if es_enriquecido:
-                        lit, cu, au = "Supergene_Chalcocite", normal_random(2.5, 0.3), log_normal_from_mean_sd(0.5, 0.1)
+                        lit = "Supergene_Chalcocite"
+                        cu = np.clip(normal_random(2.5, 0.3), 0.5, 2.5)
+                        au = np.clip(normal_random(2.5, 0.5), 2.0, 5.0)
                     else:
                         lit = "Quartz_Vein_Core" if "Tabular" in tipo_yacimiento else "Massive_Body_Core"
-                        cu = normal_random(1.2, 0.15) * (factor_forma + 0.2)
-                        au = log_normal_from_mean_sd(0.6, 0.1) * (factor_forma + 0.2)
+                        cu = np.clip(normal_random(1.2, 0.45), 0.5, 2.5)
+                        au = np.clip(normal_random(2.5, 0.65), 2.0, 5.0)
                 elif factor_forma > 0.12:
                     if es_enriquecido:
                         lit, cu, au = "Enriched_Halo", normal_random(0.9, 0.15), log_normal_from_mean_sd(0.15, 0.04)
                     else:
-                        lit = "Stockwork_Halo" if "Tabular" in tipo_yacimiento else "Mineralized_Breccia"
-                        #cu = normal_random(1.2, 0.15) * (factor_forma + 0.2)
-			cu = np.clip(normal_random(1.2, 0.45), 0.5, 2.5)
-                        #au = log_normal_from_mean_sd(0.15, 0.05) * (factor_forma + 0.1)
-			au = np.clip(normal_random(2.5, 0.65), 2.0, 5.0)
+                          lit = "Stockwork_Halo" if "Tabular" in tipo_yacimiento else "Mineralized_Breccia"
+                        cu = np.clip(normal_random(0.6, 0.2), 0.15, 1.2)
+                        au = np.clip(normal_random(1.2, 0.4), 0.5, 2.0)
 
                 else:
                     lit, cu, au = "Country_Rock", np.random.rand() * 0.05, np.random.rand() * 0.02
